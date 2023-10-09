deaa0e652d021f060230edea31b51ec9bc9ea310
Sammy Mulandi <mulandisammy419@gmail.com>
2023-10-09 13:39:38 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,67 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+
 /**
- * init_dog - Initialize a variable of type struct dog.
- * @d: Variable to initialize.
- * @name: Dog's name.
- * @age: Dog's age.
- * @owner: Dog's owner.
+ * _strlen - counts length of a string
+ * @s: pointer to string
+ * Return: size of string
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+int _strlen(char *s)
 {
-	if (d != NULL)
+	int size = 0;
+
+	while (*s++)
+		size++;
+
+	return (size);
+}
+
+
+/**
+ * *new_dog - creates a new struct and copies values
+ * @name: points to the members `name` value
+ * @age: members `age` value
+ * @owner: points to the members `owner` value
+ * Return: pointer to new struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, name_size = 0, owner_size = 0;
+	char *temp_name, *temp_owner;
+	dog_t *new_struct;
+
+	new_struct = malloc(sizeof(dog_t)); /* malloc & check new struct */
+	if (new_struct == NULL)
+		return (NULL);
+
+	name_size = _strlen(name); /* det. length of data to store */
+	owner_size = _strlen(owner);
+
+	temp_name = malloc(sizeof(char) * name_size + 1); /* store `name` */
+	if (temp_name == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		free(new_struct);
+		return (NULL);
 	}
+
+	for (i = 0; i < name_size; i++)
+		temp_name[i] = name[i];
+	temp_name[i] = '\0';
+
+	temp_owner = malloc(sizeof(char) * owner_size + 1); /* store `owner` */
+	if (temp_owner == NULL)
+	{
+		free(temp_name);
+		free(new_struct);
+		return (NULL);
+	}
+
+	for (i = 0; i < owner_size; i++)
+		temp_owner[i] = owner[i];
+	temp_owner[i] = '\0';
+
+	new_struct->name = temp_name; /* copy contents to `new_struct` and return */
+	new_struct->age = age;
+	new_struct->owner = temp_owner;
+	return (new_struct);
 }
