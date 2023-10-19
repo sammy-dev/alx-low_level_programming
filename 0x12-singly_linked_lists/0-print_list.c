98bd9b6d1655e114b9996fecc8bded2f201da014
Sammy Mulandi <mulandisammy419@gmail.com>
2023-10-19 12:06:51 -0700

    0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,8 +12,8 @@ size_t print_list(const list_t *h)
   size_t node_count = 0;
   const list_t *current = h;
 
-  while (current != NULL){
-    if (current->str == NULL){
+  while (current){
+    if (!current->str){
       printf ("[0] (nil)\n");
     }else {
       printf ("[%u] %s\n", current->len, current->str);
