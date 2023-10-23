0f796302c2b1a60d77cae6de401eb6ce0f756022
Sammy Mulandi <mulandisammy419@gmail.com>
2023-10-23 05:07:49 -0700

    1-listint_len.c file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * list_length - returns the number of elements in a linked listint_t list.
+ * @h: linked list of type listint_t to traverse
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t number = 0;
+while (h ! = NULL)
+{
+number++;
+h = h->next;
+}
+return(number);
+}
